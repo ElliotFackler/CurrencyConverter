01efb83cf633d9281023d9116e222c8e6a4e0372
Elliot Fackler <grant.fackler48@gmail.com>
2025-11-10 19:17:59 -0600

    Added functions and fixed conversion

--- Converter.java
+++ Converter.java
@@ -1,14 +1,19 @@
 import java.util.Map;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+//import com.google.gson.Gson; // Required for JSON parsing
+//import com.google.gson.reflect.TypeToken; // Helper for Gson
 
 
 
 public class Converter {
+    private static final String API_KEY = "e247acbf326ebef115b500f3";
+    private static final String API_URL = "";
     public static void main(String[] args) {
         String currencyFrom;
         String currencyTo;
@@ -41,7 +46,6 @@ public class Converter {
                 }
                 scanner.close();
 
-
                 // Define the end value.
                 double endAmount = 0;
 
@@ -54,20 +58,26 @@ public class Converter {
 
 
         // Get conversion
-        if (currencyFrom != standardCurrency & currencyTo == standardCurrency) {
-            endAmount = amount / exchangeRates.get(currencyTo);
-        } else if (currencyFrom != standardCurrency && currencyTo != standardCurrency) {
-            endAmount = amount / exchangeRates.get(currencyTo);
-            endAmount = endAmount * exchangeRates.get(currencyFrom);
-        } else if (currencyFrom == standardCurrency) {
-            endAmount = amount * exchangeRates.get(currencyFrom);
+        if (!currencyFrom.equals(standardCurrency) && currencyTo.equals(standardCurrency)) {
+            endAmount = divide(amount, exchangeRates.get(currencyFrom));
+        } else if (!currencyFrom.equals(standardCurrency) && !currencyTo.equals(standardCurrency)) {
+            endAmount = divide(amount, exchangeRates.get(currencyFrom));
+            endAmount = multiple(endAmount, exchangeRates.get(currencyTo));
+        } else {
+            endAmount = multiple(amount, exchangeRates.get(currencyTo));
         }
-
-        
 
         // Return conversion result
         System.out.printf("Your %s %.2f is equivalent to %s %.2f", currencyFrom, amount, currencyTo, endAmount);
         
         return;
+    }
+
+    public static double divide(double num, double den) {
+        return num / den;
+    }
+
+    public static double multiple(double num1, double num2) {
+        return num1 * num2;
     }
 }
